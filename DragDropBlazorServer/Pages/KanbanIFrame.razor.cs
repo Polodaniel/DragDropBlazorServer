731906f89400d24bfdce267d7fcb6a357866ae17
Daniel Polo <daniel_polo@live.com>
2021-09-28 20:27:53 -0300

    Correção do iFrame

---
+++ DragDropBlazorServer/Pages/KanbanIFrame.razor.cs
@@ -0,0 +1,63 @@
+﻿using DragDropBlazorServer.Data;
+using DragDropBlazorServer.Shared.Dialog;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragDropBlazorServer.Pages
+{
+    public class KanbanIFrameBase : ComponentBase
+    {
+        [Inject]
+        public IDialogService DialogService { get; set; }
+
+        public List<ItemAtividade> ListaAtividade { get; set; }
+
+        protected string UltimaAtividadeAtualizada { get; set; }
+
+        public KanbanIFrameBase()
+        {
+            ListaAtividade = new List<ItemAtividade>();
+            UltimaAtividadeAtualizada = string.Empty;
+        }
+
+        protected override void OnInitialized()
+        {
+
+        }
+
+        public void StatusUpdate(ItemAtividade atividade)
+        {
+            UltimaAtividadeAtualizada = atividade.Titulo;
+        }
+
+        public async void NovaAtividade()
+        {
+            var ParametersDialog = new DialogParameters();
+
+            var OptionDialog = new DialogOptions();
+            OptionDialog.MaxWidth = MaxWidth.Medium;
+            OptionDialog.CloseButton = false;
+            OptionDialog.DisableBackdropClick = true;
+            OptionDialog.Position = DialogPosition.Center;
+
+            var Dialog = DialogService.Show<NovaAtividadeDialog>($"Nova Atividade", ParametersDialog, OptionDialog);
+            var Result = await Dialog.Result;
+
+            if (!Result.Cancelled)
+            {
+                var NovaAtividade = (ItemAtividade)Result.Data;
+
+                if (!Equals(NovaAtividade, null))
+                {
+                    ListaAtividade.Add(NovaAtividade);
+
+                    StateHasChanged();
+                }
+            }
+        }
+    }
+}
